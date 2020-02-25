richfelker/cowpatch

commit 2516065fc3958b5eb32d4a165fa2f11d7c440a04

add -I and -S options to initialize tree, split file; document them

cowpatch.sh
@@ -88,4 +88,13 @@ done
 
 }
 
+gotcmd=0
+while getopts ":p:i:RNEI:S:" opt ; do
+case "$opt" in
+I) find "$OPTARG" -path "$OPTARG/*" -prune -exec sh -c 'ln -sf "$@" .' sh {} + ; gotcmd=1 ;;
+S) cowp "$OPTARG" ; gotcmd=1 ;;
+esac
+done
+test "$gotcmd" -eq 0 || exit 0
+
 cowpatch "$@" | patch "$@"

commit 1db812b0ca94d07ede9891883484f2a76dae8e23

fix regression with some shells due to unspecified behavior
commit 2516065fc3958b5eb32d4a165fa2f11d7c440a04 introduced unspecified
behavior by using getopts twice without resetting OPTIND. per POSIX:

    "If the application sets OPTIND to the value 1, a new set of
    parameters can be used: either the current positional parameters
    or new arg values. Any other attempt to invoke getopts multiple
    times in a single shell execution environment with parameters
    (positional parameters or arg operands) that are not the same in
    all invocations, or with an OPTIND value modified to be a value
    other than 1, produces unspecified results."

this broke at least on bash and possibly other shells.

cowpatch.sh
@@ -54,6 +54,7 @@ done
 cowpatch () {
 
 plev=0
+OPTIND=1
 while getopts ":p:i:RNE" opt ; do
 test "$opt" = p && plev="$OPTARG"
 done